4878388af6cd96bebecdc307d006622aa87f9571
kylecuberg <kyle.patterson@cuberg.net>
2022-07-14 17:19:59 -0700

    update tests

--- Tests/test_pytemplate.py
+++ Tests/test_pytemplate.py
@@ -2,12 +2,18 @@
 import time
 
 # First-party/Local
+import src.core
 import src.util
 from src import __version__
 
 
 def test_version():
-    assert __version__ == "0.2.0"
+    assert __version__ == "0.2.1"
+
+
+def test_core():
+    # Pass
+    assert src.core.main() is None
 
 
 def test_util_seconds_but_readable():
--- src/core.py
+++ src/core.py
@@ -5,7 +5,6 @@ def main():
         True
     except Exception as E:
         print(type(E).__name__, __file__, E.__traceback__.tb_lineno, "\n", E)
-    return None
 
 
 if __name__ == "__main__":
